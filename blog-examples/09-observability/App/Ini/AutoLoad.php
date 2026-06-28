0048aa27c517e9953c32e08f966ac3aa3ed775b7
Ibrahim BinAlshikh <ibinalshikh@hotmail.com>
2026-06-28 17:24:08 +0300

    Remove manual health check registration, rely on auto-discovery

--- blog-examples/09-observability/App/Ini/AutoLoad.php
+++ blog-examples/09-observability/App/Ini/AutoLoad.php
@@ -2,10 +2,6 @@
 
 namespace App\Ini;
 
-use App\Health\DatabaseCheck;
-use App\Health\PaymentGatewayCheck;
-use WebFiori\Framework\Health\Checks\CacheCheck;
-use WebFiori\Framework\Health\Checks\StorageCheck;
 use WebFiori\Framework\Health\HealthCheck;
 use WebFiori\Log\FileLogger;
 use WebFiori\Log\LoggerFacade;
@@ -16,12 +12,6 @@ class AutoLoad {
         // Configure logging
         $logDir = dirname(__DIR__) . '/Storage/Logs';
         LoggerFacade::setInstance(new FileLogger($logDir, LogLevel::DEBUG));
-
-        // Register health checks
-        HealthCheck::register(new DatabaseCheck());
-        HealthCheck::register(new PaymentGatewayCheck());
-        HealthCheck::register(new StorageCheck());
-        HealthCheck::register(new CacheCheck());
 
         // Log health check failures
         HealthCheck::afterAll(function (array $result) {
